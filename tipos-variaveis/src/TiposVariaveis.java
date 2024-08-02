dff5a3efd503f78d3c65270cc8bb0a69660402d1
Anderson Santos <anderson.santos.silva@live.com>
2024-08-02 20:46:23 -0300

    Estudo de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        short numeroCurto = 1;
+        int numetoNormal = numeroCurto;
+        int numeroCurto2 = numetoNormal;
+
+        int numero = 5;
+        numero = 100;
+        System.out.print(numero);
+
+
+        final double VALOR_PI = 3.14;
+
+        System.out.print(VALOR_PI);
+
+    
     }
 }
